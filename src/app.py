4a1b1b25b7d287349e342fcd8e644310d5b6400d
darrida <darrida.py@gmail.com>
2024-03-03 22:36:33 -0600

    main view that starts hidden

--- src/app.py
+++ src/app.py
@@ -22,6 +22,13 @@ app_ui = ui.page_sidebar(
     )
 
 
+def app_view():
+    return ui.div(
+        ui.input_text("test_box", "Test Box"),
+        ui.input_action_button("click_me", "Click Me", width="100px")
+    )
+
+
 def server(input: Inputs, output: Outputs, session: Session):
     #################################################################################################
     # AUTH MODULE RELATED
@@ -29,6 +36,12 @@ def server(input: Inputs, output: Outputs, session: Session):
     
     session_auth = shiny_auth.AuthReactiveValues
     shiny_auth.server("shiny_auth_module", session_auth)
+
+    @render.ui
+    def main_view():
+        if session_auth.hide_app.get() is True:
+            return
+        return app_view()
 
 
 app = App(app_ui, server)
